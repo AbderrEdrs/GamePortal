95fa440a91fe508fd720a185d75782fcb351cf60
Lakhal Dhia-elhak <lakhal@b11p2.fil.univ-lille1.fr>
2013-12-19 10:15:47 +0100

    js

--- src/factoryfun/controller/FactoryController.java
+++ src/factoryfun/controller/FactoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import factoryfun.donnees.JoueurProvider;
 
@@ -31,6 +32,14 @@ public class FactoryController
 		//model.addAttribute("plateau", init.getJoueur().getPlateau());
 		
 		return "factory/test";
+	}
+	
+	@RequestMapping("factoryfun/action")
+	public String action(Model model, @RequestParam("name") String name)
+	{
+		System.out.println(name);
+		
+		return "";
 	}
 	
 }
